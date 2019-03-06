bffebe2e35688f1fdc868c79c164755c84e482e4
sanghee <holash@likelion.org>
2019-03-06 23:06:39 +0900

    delete and pagination and photo

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     body = models.TextField()
+    photo = models.ImageField(blank=True,upload_to="blog/%Y/%m/%d")
 
     def __str__(self):
         return self.title
